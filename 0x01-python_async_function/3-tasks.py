f0c9dab6c7d26522f076dd4792ff886b4754182e
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-14 21:08:04 +0100

    task3

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Regular function returning asyncio.Task
+"""
+import time
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> asyncio.Task:
+    """
+    Calls wait_random and returns it as a created task
+    :param max_delay: max delay to wait before returning
+    :return: a Task
+    """
+    return asyncio.create_task(wait_random(max_delay))
