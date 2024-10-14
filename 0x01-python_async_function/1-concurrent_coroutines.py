a56b63b381fa05199c79e6b54b106def3b865473
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-14 20:53:03 +0100

    test

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,11 +5,12 @@ of each coroutine in a list and returns it
 """
 import asyncio
 from asyncio import as_completed
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     performs n calls of the wait_random method,
     stores the return value of each call in a list,
@@ -18,7 +19,7 @@ async def wait_n(n: int, max_delay: int) -> list:
     :param max_delay: maximum delay of async coroutine
     :return: list of delays
     """
-    tasks: list[asyncio.Task] = [asyncio.create_task(wait_random(max_delay))
+    tasks: List[asyncio.Task] = [asyncio.create_task(wait_random(max_delay))
                                  for _ in range(n)]
     delays: list = []
     for task in as_completed(tasks):
