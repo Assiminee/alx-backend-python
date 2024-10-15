5ac53ee537013c33209faa9e26f13f1b87b396e4
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-10-15 23:07:45 +0100

    edits

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,24 +4,14 @@ Contains an async generator function
 """
 import asyncio
 import random
-from typing import Generator, List
+from typing import Generator
 
 
-async def generate() -> Generator[int, any, any]:
+async def async_generator() -> Generator[float, any, any]:
     """
     coroutine that loops 10 times, each time asynchronously waits 1 second,
     then yields a random number between 0 and 10
-    :return:
     """
-    for i in range(10):
+    for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 5)
-
-
-async def async_generator() -> List[int]:
-    """
-    returns a list of random numbers using list comprehension
-    in asynchronous methods
-    :return:
-    """
-    return [i async for i in generate()]
+        yield random.uniform(0, 10)
