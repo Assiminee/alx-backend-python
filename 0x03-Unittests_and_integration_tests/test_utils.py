f32b1c949a67c7e7eb12f2a77ef9558282a8aa4d
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-06 23:40:23 +0100

    task3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,8 +4,9 @@ Module defining tests for
 """
 import unittest
 from typing import Tuple, Dict, Union
+from unittest.mock import patch, Mock
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -41,5 +42,29 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
 
-if __name__ == "__main__":
-    unittest.main()
+
+class TestGetJson(unittest.TestCase):
+    """
+    Test class for the method get_json
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch("requests.get")
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """
+        Tests method get_json using parameters
+        passed using parameterized.expand
+        """
+        mock_response = Mock()
+
+        mock_response.url = test_url
+        mock_response.json.return_value = test_payload
+
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+        mock_get.assert_called_once_with(test_url)
+
+        self.assertEqual(test_payload, result)
