1a98e085c48244da312b2f025ef6e85800679a74
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-08 21:07:04 +0100

    modified test memoize

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -88,9 +88,12 @@ class TestMemoize(unittest.TestCase):
         with patch.object(
                 TestClass,
                 "a_method",
-                return_value=lambda: 42,
         ) as memo_fn:
+            memo_fn.return_value = 42
             test_class = TestClass()
-            self.assertEqual(test_class.a_property(), 42)
-            self.assertEqual(test_class.a_property(), 42)
+            self.assertEqual(test_class.a_property, 42)
+            self.assertEqual(test_class.a_property, 42)
             memo_fn.assert_called_once()
+
+if __name__ == "__main__":
+    unittest.main()
