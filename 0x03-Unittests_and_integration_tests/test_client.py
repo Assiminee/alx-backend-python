9858e2c3083943b3f004b82eb97ca0a55006fe8e
Yasmine Znatni <znatni.yasmine@gmail.com>
2024-11-08 22:23:01 +0100

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -34,3 +34,58 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_org.return_value = {"repos_url": "value"}
             goc = GithubOrgClient("org_name")
             self.assertEqual(goc._public_repos_url, "value")
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """ method to unit-test GithubOrgClient.public_repos """
+        test_payload = {
+            'repos_url': "https://api.github.com/users/assimine/repos",
+            'repos': [
+                {
+                    "id": 1918182,
+                    "name": "assimine.repo.1",
+                    "private": True,
+                    "owner": {
+                        "login": "assimine",
+                        "id": 92039,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/assimine/assimine.repo.1",
+                    "created_at": "2022-11-01T00:31:37Z",
+                    "updated_at": "2023-09-23T11:53:58Z",
+                    "has_issues": False,
+                    "forks": 22,
+                    "default_branch": "master",
+                },
+                {
+                    "id": 111111,
+                    "name": "assimine.repo.2",
+                    "private": False,
+                    "owner": {
+                        "login": "assimine",
+                        "id": 92039,
+                    },
+                    "fork": False,
+                    "url": "https://api.github.com/repos/assimine/assimine.repo.2",
+                    "created_at": "2020-03-04T22:52:33Z",
+                    "updated_at": "2024-11-15T22:22:16Z",
+                    "has_issues": True,
+                    "forks": 101,
+                    "default_branch": "master",
+                },
+            ]
+        }
+        mock_get_json.return_value = test_payload["repos"]
+        with patch.object(
+                GithubOrgClient, "_public_repos_url", new_callable=PropertyMock
+        ) as mock_pub:
+            mock_pub.return_value = test_payload["repos_url"]
+            self.assertEqual(
+                GithubOrgClient("assimine").public_repos(),
+                [
+                    "assimine.repo.1",
+                    "assimine.repo.2"
+                ],
+            )
+            mock_pub.assert_called_once()
+            mock_get_json.assert_called_once()
